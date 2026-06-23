ca8fee4e03469b14ba45a87b96227be0195652ac
sofidt17 <sofiadellatorre1@gmail.com>
2026-06-23 19:01:44 -0300

    ahora la reina se queda un tiempo parada cuando le revolea la tira de asado

--- Assets/Scripts/PersecucionReina.cs
+++ Assets/Scripts/PersecucionReina.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 public class PersecucionReina : MonoBehaviour
 {
     [Header("Configuración")]
-    public Transform objetivo; // Acá vamos a arrastrar a Máxima
+    public Transform objetivo;
     public float velocidad = 3.5f;
-    public float distanciaMinima = 1f; // Distancia que mantiene con Máxima
+    public float distanciaMinima = 1f;
 
     private Rigidbody2D rb;
     private SpriteRenderer spriteRenderer;
+    private bool comiendo = false;
 
     void Start()
     {
@@ -20,8 +21,14 @@ public class PersecucionReina : MonoBehaviour
     {
         if (objetivo == null) return;
 
-        // Si Máxima murió, la Reina deja de moverse
+        if (comiendo)
+        {
+            rb.linearVelocity = Vector2.zero;
+            return;
+        }
+
         ControlMaxima control = objetivo.GetComponent<ControlMaxima>();
+
         if (control != null && !control.enabled)
         {
             rb.linearVelocity = Vector2.zero;
@@ -34,21 +41,36 @@ public class PersecucionReina : MonoBehaviour
         float distancia = Vector2.Distance(transform.position, objetivo.position);
 
         if (distancia > distanciaMinima)
-        {
             rb.linearVelocity = new Vector2(direccion.x * velocidad, 0);
-        }
         else
-        {
             rb.linearVelocity = Vector2.zero;
-        }
 
         if (direccion.x > 0)
-        {
             spriteRenderer.flipX = false;
-        }
         else if (direccion.x < 0)
-        {
             spriteRenderer.flipX = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("La Reina tocó: " + other.gameObject.name);
+
+        if (other.CompareTag("Asado"))
+        {
+            Debug.Log("La Reina encontró el asado");
+
+            Destroy(other.gameObject);
+
+            StartCoroutine(ComerAsado());
         }
+    }
+
+    private System.Collections.IEnumerator ComerAsado()
+    {
+        comiendo = true;
+
+        yield return new WaitForSeconds(4f);
+
+        comiendo = false;
     }
 }
