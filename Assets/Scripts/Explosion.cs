5748110ef62460f45b4468f15e25f479bbb822d0
sofidt17 <sofiadellatorre1@gmail.com>
2026-07-21 07:44:47 -0300

    bombas explotan y hacen daño

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -2,8 +2,25 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
+    [Header("Daño")]
+    public float tiempoVida = 0.4f;
+
+    private bool hizoDaño = false;
+
     void Start()
     {
-        Destroy(gameObject, 0.4f);
+        Destroy(gameObject, tiempoVida);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (hizoDaño)
+            return;
+
+        if (other.CompareTag("Player"))
+        {
+            hizoDaño = true;
+            GestorVidas.instancia.PerderVida(other.gameObject);
+        }
     }
 }
