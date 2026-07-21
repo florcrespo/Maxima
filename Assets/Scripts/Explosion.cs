c11438befb1d73ab8b0a09b73fb6b13300b74986
sofidt17 <sofiadellatorre1@gmail.com>
2026-07-21 07:49:31 -0300

    sonido bombas

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -5,10 +5,20 @@ public class Explosion : MonoBehaviour
     [Header("Daño")]
     public float tiempoVida = 0.4f;
 
+    [Header("Sonido")]
+    public float volumen = 2f;
+
     private bool hizoDaño = false;
 
     void Start()
     {
+        AudioClip clip = Resources.Load<AudioClip>("explosion");
+
+        if (clip != null)
+        {
+            AudioSource.PlayClipAtPoint(clip, transform.position, volumen);
+        }
+
         Destroy(gameObject, tiempoVida);
     }
 
